468a9b6b327c30b9f6323790a9cf5f097ba2abc1
Caleb Owens <owenscaleb75@gmail.com>
2021-03-08 17:30:35 -0600

    minor edits to Orders

--- BestEats.Web/wwwroot/JS/Order.js
+++ BestEats.Web/wwwroot/JS/Order.js
@@ -1,28 +1,90 @@
-﻿jQuery("#Northerville").click(function (e) {
+﻿
+let storeSelectedID;
+let productSelectedID;
+
+let currentCustomer = sessionStorage.getItem("fullName");
+
+
+
+
+jQuery("#Northerville").click(function (e) {
     
-    alert(`Showing Northerville`);
+    storeSelectedID = 1;
     e.preventDefault();
 });
 
 jQuery("#Westerville").click(function (e) {
 
-    alert("Showing Westerville");
+    storeSelectedID = 2;
     e.preventDefault();
 });
 
 jQuery("#Southerville").click(function (e) {
 
-    alert(`Showing Southerville`);
+    storeSelectedID = 3;
     e.preventDefault();
 });
 jQuery("#Easterville").click(function (e) {
 
-    alert(`Showing Easterville`);
+    storeSelectedID = 4;
     e.preventDefault();
 });
 
-//testing pipeline
+
+
+// item selection
+
+jQuery("#Apple").click(function (e) {
+
+    productSelectedID = 1;
+    e.preventDefault();
+});
+
+jQuery("#Orange").click(function (e) {
+
+    productSelectedID = 2;
+    e.preventDefault();
+});
+
+jQuery("#Banana").click(function (e) {
+
+    productSelectedID = 3;
+    e.preventDefault();
+});
+
+
 /*
+$("#storeDrop").find('li').click(function (e) {
+
+    storeSelectedID = $(this).id();
+    alert(`${storeSelectedID}`);
+    e.preventDefault();
+
+});
+*/
+
+
+
+/*
+ public void AddOrder(BestEats.Logic.Order DBorder)
+{
+    Order order = new Order
+    {
+        OrderId = DBorder.OrderId,
+            CustomerId = DBorder.CustomerId,
+            StoreId = DBorder.StoreId,
+            ProductId = DBorder.ProductId,
+            ItemName = DBorder.ItemName,
+            ProductQuantity = DBorder.ProductQuantity,
+            OrderPurchaseDate = DBorder.OrderPurchaseDate
+
+    };
+    _context.Add(order);
+}
+
+
+//testing pipeline
+
 let storeSelect = document.getElementById('storeDropDownSelection');
 storeSelect.onclick = alert(`Showing ${storeSelect}`);
 */
